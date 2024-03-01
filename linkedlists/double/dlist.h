009f65e86e091e7df99ce7106befa680459c8e17
julissaehp <101850592+julissaehp@users.noreply.github.com>
2024-03-01 09:18:35 -0500

    add dlist_length

---
+++ linkedlists/double/dlist.h
@@ -0,0 +1,12 @@
+typedef struct dnode {
+    int val;
+    struct dnode* next;
+    struct dnode* prev;
+} Dnode;
+
+Dnode* make_dnode(int);
+char* print_dlist(Dnode*);
+void remove_from_dlist(Dnode**, Dnode*);
+void dlist_insert_at_index(Dnode**, int, int);
+Dnode* find_in_dlist(Dnode*, int);
+int dlist_length(Dnode* head);
