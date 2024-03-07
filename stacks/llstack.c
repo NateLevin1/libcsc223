ce52faa3247a0d2c121fcc4e36fe674cdde49005
NateLevin1 <63370104+UltimatePro-Grammer@users.noreply.github.com>
2024-03-07 08:44:17 -0500

    ✨ Implement llstack

--- stacks/llstack.c
+++ stacks/llstack.c
@@ -14,14 +14,37 @@ StackNode* new_node(int data) {
     return new;
 }
 
-StackNode* pop(StackNode** stack) {
-    return NULL;
+StackNode* new_llstack(int data) {
+    return new_node(data);
 }
 
-StackNode* top(StackNode** stack) {
-    return NULL;
+int llstack_is_empty(StackNode** stack) {
+    return (*stack) == NULL;
 }
 
-void push(StackNode** stack, StackNode*) {
+int llstack_pop(StackNode** stack) {
+    if ((*stack) == NULL) {
+        printf("Underflow: tried to pop an empty stack\n");
+        exit(1);
+    }
+    StackNode* old_top = *stack;
+    int old_top_val = old_top->val;
+    *stack = old_top->next;
+    free(old_top);
+    return old_top_val;
+}
+
+int llstack_top(StackNode** stack) {
+    return (*stack)->val;
+}
+
+void llstack_push(StackNode** stack, int val) {
+    StackNode* node = new_node(val);
+    if (stack == NULL || (*stack) == NULL) {
+        *stack = node;
+    } else {
+        node->next = *stack;
+        *stack = node;
+    }
 }
 
--- stacks/test_llstack.c
+++ stacks/test_llstack.c
@@ -1,9 +1,26 @@
+#include "llstack.h"
 #include <ctest.h>
 
 int main() {
     BEGIN_TESTING("llstack.h");
 
+    TEST("stack operations work") {
+        Stack stack = new_llstack(1);
+        ASSERT_EQ(llstack_top(&stack), 1);
 
+        llstack_push(&stack, 2);
+        ASSERT_EQ(llstack_top(&stack), 2);
+
+        llstack_pop(&stack);
+        ASSERT_EQ(llstack_top(&stack), 1);
+
+        llstack_pop(&stack);
+        ASSERT(llstack_is_empty(&stack));
+    }
+
+    TEST("stack operations return the correct values") {
+
+    }
 
     END_TESTING();
 }
