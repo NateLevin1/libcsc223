9a7c7331c4d4ff5762b8ddd87e2ac690a1c29a84
Joseph Sparks <87674378+JosephSparks@users.noreply.github.com>
2024-03-10 22:00:49 -0400

    Add print_dlist and llstack pop test case (#8)
    * Creating print_dlist
    
    * Update dlist.c
    
    * Updated insert_in_order
    
    * Upstream merge + resolved commits w/ Nate
    
    * Create pop test
    
    * complete pop test case

--- linkedlists/double/dlist.c
+++ linkedlists/double/dlist.c
@@ -15,6 +15,66 @@ Dnode* make_dnode(int data) {
     return new;
 }
 
+char* print_dlist(Dnode* head) {
+    char* result = malloc(1024);
+
+    int index = 0;
+    Dnode* Dnode = head;
+    while (Dnode != NULL) {
+        int num_len = snprintf(NULL, 0, "%d", Dnode->val);
+        snprintf(result + index, 8, "%d", Dnode->val);
+        index += num_len - 1;
+        if (Dnode->next != NULL) {
+            result[++index] = ' ';
+            result[++index] = '-';
+            result[++index] = '>';
+            result[++index] = ' ';
+        }
+        index++;
+
+        Dnode = Dnode->next;
+    }
+    result[index] = '\0';
+
+    return result;
+}
+
+void dlist_insert_in_order(Dnode** list, Dnode** newDnode){
+
+    Dnode* current;
+
+    if(*list == NULL){
+        *list = (*newDnode);
+    }
+
+    else if((*list)->next->val >= (*newDnode)->val){
+        (*newDnode)->next = *list;
+        (*newDnode)->next->prev = (*newDnode);
+        *list = (*newDnode);
+    }
+
+    else { 
+        current = *list;
+  
+        // locate the node after which the new node 
+        // is to be inserted 
+        while (current->next != NULL &&  
+               current->next->val < (*newDnode)->val) 
+            current = current->next;
+  
+        /* Make the appropriate links */
+        (*newDnode)->next = current->next;
+  
+        // if the new node is not inserted 
+        // at the end of the list 
+        if (current->next != NULL) 
+            (*newDnode)->next->prev = *newDnode;
+  
+        current->next = *newDnode;
+        (*newDnode)->prev = current;
+    } 
+} 
+
 void remove_from_dlist(Dnode** head_ref, Dnode* del_node) {
     // Base case: If the list is empty or del_node is NULL, return
     if (*head_ref == NULL || del_node == NULL)
