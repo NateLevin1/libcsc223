2793102548a74c873bf2db8563cef107894d53f1
NateLevin1 <63370104+UltimatePro-Grammer@users.noreply.github.com>
2024-03-13 08:36:32 -0400

    Implement Towers of Hanoi

--- stacks/applications/towersofhanoi.c
+++ stacks/applications/towersofhanoi.c
@@ -1,5 +1,30 @@
 #include <ctest.h>
 #include "../llstack.h"
 
+void move(int n, Stack* source, Stack* dest, Stack* spare) {
+    if (n == 1) {
+        printf("Moving val %d from %p to %p\n", llstack_top(source), source, dest);
+        llstack_push(dest, llstack_pop(source));
+        return;
+    }
+    move(n - 1, source, spare, dest);
+    move(1, source, dest, spare);
+    move(n - 1, spare, dest, source);
+}
+
 int main() {
+    TEST("Towers of Hanoi") {
+        Stack source = new_llstack_node(3);
+        llstack_push(&source, 2);
+        llstack_push(&source, 1);
+
+        Stack dest = NULL;
+        Stack spare = NULL;
+
+        move(3, &source, &dest, &spare);
+
+        ASSERT(llstack_is_empty(&source));
+        ASSERT(llstack_is_empty(&spare));
+        ASSERT_EQ(llstack_top(&dest), 1);
+    }
 }
