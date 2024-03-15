94770a3bbb82dde9519e2fc87b7f0d115357134b
julissaehp <101850592+julissaehp@users.noreply.github.com>
2024-03-15 08:37:57 -0400

    Implement parens checker

--- stacks/applications/parenschecker.c
+++ stacks/applications/parenschecker.c
@@ -1,5 +1,45 @@
 #include <ctest.h>
 #include "../llstack.h"
+#include <stdbool.h> 
+#include <stdio.h>
 
-int main() {
+bool isBalanced(char exp[])
+{
+    bool flag = true;
+    int count = 0;
+ 
+    for (int i = 0; exp[i] != '\0'; i++) {
+ 
+        if (exp[i] == '(') {
+            count++;
+        }
+        else {
+            count--;
+        }
+        if (count < 0) {
+            flag = false;
+            break;
+        }
+    }
+    if (count != 0) {
+        flag = false;
+    }
+ 
+    return flag;
+}
+
+int main(){
+    BEGIN_TESTING("parenschecker.c");
+
+    TEST("isBalanced"){
+        ASSERT_EQ(isBalanced("()"),  true);
+        ASSERT_EQ(isBalanced("()("), false);
+        ASSERT_EQ(isBalanced("()()"), true);
+        ASSERT_EQ(isBalanced("([)]"), false);
+        ASSERT_EQ(isBalanced("{[]}"), false);
+
+        printf("All tests passed successfully!\n");
+    }
+   
+    END_TESTING();
 }
