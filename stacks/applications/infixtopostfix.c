096afef33d8962ebb38d83d0b64150d2c9b07998
Sheel2007 <sheel_arsenal@icloud.com>
2024-03-19 09:02:36 -0400

    Commented infixtopostfix() and infixtoprefix()

--- stacks/applications/infixtopostfix.c
+++ stacks/applications/infixtopostfix.c
@@ -4,6 +4,7 @@
 #include <ctest.h>
 #include "../llstack.h"
 
+// Function to determine precedence of operators
 int precedence(char op) {
     switch (op) {
         case '+':
@@ -14,49 +15,64 @@ int precedence(char op) {
         case '%':
             return 2;
         default:
-            return 0;
+            return 0; // Default precedence for other characters
     }
 }
 
+// Function to convert infix expression to postfix expression
 void infixToPostfix(char* infix, char* postfix) {
-    StackNode* stack = NULL;
+    StackNode* stack = NULL; // Initialize an empty stack
     int i = 0, j = 0;
     char token, popped;
 
+    // Push '(' onto the stack to handle expressions starting with '('
     llstack_push(&stack, '(');
+
+    // Append ')' to the infix expression to mark the end
     strcat(infix, ")");
 
+    // Iterate through each character of the infix expression
     while (infix[i] != '\0') {
         token = infix[i];
 
+        // If the current token is '(', push it onto the stack
         if (token == '(') {
             llstack_push(&stack, token);
-        } else if ((token >= 'A' && token <= 'Z') || (token >= 'a' && token <= 'z')) {
+        }
+        // If the token is an operand (alphabet), add it to the postfix expression
+        else if ((token >= 'A' && token <= 'Z') || (token >= 'a' && token <= 'z')) {
             postfix[j++] = token;
-        } else if (token == ')') {
+        }
+        /* If the scanned character is an ‘)’, 
+        pop and add to the output string from the stack
+        until an ‘(‘ is encountered.*/
+        else if (token == ')') {
             while ((popped = llstack_pop(&stack)) != '(') {
                 postfix[j++] = popped;
             }
-        } else { // token is an operator
+        }
+        // If the token is an operator
+        else { 
+            // Pop operators from the stack with higher or equal precedence and append to postfix
             while (!llstack_is_empty(&stack) && llstack_top(&stack) != '(' && precedence(token) <= precedence(llstack_top(&stack))) {
                 popped = llstack_pop(&stack);
                 postfix[j++] = popped;
             }
+            // Push the current operator onto the stack
             llstack_push(&stack, token);
         }
-        i++;
+        i++; // Move to the next character in the infix expression
     }
-    postfix[j] = '\0';
+    postfix[j] = '\0'; // Null-terminate the postfix expression
 }
 
 int main() {
-
-
+    // Test case using CTest framework
     BEGIN_TESTING("infixtopostfix.c");
 
     TEST("Converts infix expression to postfix") {
-        char infix[] = "(A+B)*(C*D)";
-        char expected_postfix[] = "AB+CD**";
+        char infix[] = "A+B*C+D";
+        char expected_postfix[] = "ABC*+D+";
 
         char postfix[100];
         infixToPostfix(infix, postfix);
