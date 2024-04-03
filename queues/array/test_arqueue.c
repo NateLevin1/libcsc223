9d4ed47a4f4bbbc1ef4e71948e9f7e46941eec6d
NateLevin1 <63370104+UltimatePro-Grammer@users.noreply.github.com>
2024-04-03 09:04:53 -0400

    Add boilerplate

---
+++ queues/array/test_arqueue.c
@@ -0,0 +1,8 @@
+#include "arqueue.h"
+#include <ctest.h>
+
+int main() {
+    BEGIN_TESTING("arqueue.h");
+
+    END_TESTING();
+}
---
+++ queues/linkedlist/test_llqueue.c
@@ -0,0 +1,8 @@
+#include "llqueue.h"
+#include <ctest.h>
+
+int main() {
+    BEGIN_TESTING("llqueue.h");
+
+    END_TESTING();
+}
